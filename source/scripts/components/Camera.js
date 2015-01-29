89dc6cee1070ca180dd26031aac69ed65120b2fa
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-28 20:50:50 -0500

    Fixed the camera on the position of the player.

--- source/scripts/components/Camera.js
+++ source/scripts/components/Camera.js
@@ -1,10 +1,26 @@
+var PlayerStore = require("<scripts>/stores/PlayerStore")
+
 var Camera = React.createClass({
+    mixins: [
+        Reflux.connect(PlayerStore, "player")
+    ],
     render: function() {
         return (
-            <div className="camera">
-                {this.props.children}
+            <div style={this.renderStyles()}
+                 className={this.renderClasses()}>
+                 {this.props.children}
             </div>
         )
+    },
+    renderStyles: function() {
+        return {
+            left: this.state.player.position * 20 * -1 + "em"
+        }
+    },
+    renderClasses: function() {
+        return React.addons.classSet({
+            "camera": true
+        })
     }
 })
 
