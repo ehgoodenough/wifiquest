1cfc17126bd4e52c9bd8ce141407fbdd0a43471e
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-02 21:33:47 -0500

    Refactored the elements into the characters.

--- source/scripts/components/Character.js
+++ source/scripts/components/Character.js
@@ -8,14 +8,16 @@ var Character = React.createClass({
         x: React.PropTypes.number,
         y: React.PropTypes.number,
         width: React.PropTypes.number,
-        height: React.PropTypes.number
+        height: React.PropTypes.number,
+        image: React.PropTypes.string
     },
     getDefaultProps: function() {
         return {
             x: 0,
             y: 0,
             width: 1,
-            height: 1
+            height: 1,
+            image: ""
         }
     },
     render: function() {
@@ -30,7 +32,9 @@ var Character = React.createClass({
             left: (this.state.camera.x / this.props.z) + this.props.x + "em",
             top: (this.state.camera.y / this.props.z) + this.props.y + "em",
             width: this.props.width + "em",
-            height: this.props.height + "em"
+            height: this.props.height + "em",
+            backgroundImage: "url(" + this.props.image + ")",
+            backgroundColor: this.props.image == "" ? "red" : ""
         }
     },
     renderClasses: function() {
@@ -39,7 +43,9 @@ var Character = React.createClass({
         })
     },
     handleClick: function() {
-        console.log(this.props.children)
+        if(this.props.children) {
+            alert(this.props.children)
+        }
     }
 })
 
