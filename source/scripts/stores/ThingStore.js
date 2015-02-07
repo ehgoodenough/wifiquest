f4e2f271d535528c187886e470ca53bff81daa89
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-07 12:34:40 -0500

    Added an additional layer as the ground.

--- source/scripts/stores/ThingStore.js
+++ source/scripts/stores/ThingStore.js
@@ -11,19 +11,32 @@ var ThingStore = Reflux.createStore({
                 x={0} y={HEIGHT*(1/3)} z={7/2}
                 width={WIDTH*5} height={HEIGHT}
                 image={"./assets/images/background.png"}/>
+                
+            <Thing key={6}
+                x={0} y={0} z={1}
+                width={WIDTH*2} height={HEIGHT}
+                image={"./assets/images/ground1.png"}/>
+            <Thing key={7}
+                x={WIDTH*2} y={0} z={1}
+                width={WIDTH*2} height={HEIGHT}
+                image={"./assets/images/ground2.png"}/>
+            <Thing key={8}
+                x={WIDTH*4} y={0} z={1}
+                width={WIDTH*2} height={HEIGHT}
+                image={"./assets/images/ground3.png"}/>
             <Thing key={3} color="red"
-                x={WIDTH*1.5} y={HEIGHT*(2/3)} z={1}
-                width={1} height={1}>
+                x={WIDTH*1.5-0.5} y={HEIGHT-6.5} z={1}
+                width={2} height={3}>
                 Hello!
             </Thing>
             <Thing key={4} color="green"
-                x={WIDTH*2.5} y={HEIGHT*(2/3)} z={1}
-                width={1} height={1}>
+                x={WIDTH*2.5-0.5} y={HEIGHT-6.5} z={1}
+                width={2} height={3}>
                 Hiya!
             </Thing>
             <Thing key={5} color="blue"
-                x={WIDTH*3.5} y={HEIGHT*(2/3)} z={1}
-                width={1} height={1}>
+                x={WIDTH*3.5-0.5} y={HEIGHT-6.5} z={1}
+                width={2} height={3}>
                 Whatsup?
             </Thing>
         </div>
