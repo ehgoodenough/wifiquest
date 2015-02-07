d23126281fff0f24fa40ceef4cf0f0789bac68ce
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-07 12:39:11 -0500

    Added a thing for the internet machine.

--- source/scripts/stores/ThingStore.js
+++ source/scripts/stores/ThingStore.js
@@ -3,42 +3,45 @@ var Thing = require("<scripts>/components/Thing")
 var ThingStore = Reflux.createStore({
     data: (
         <div className="all-things">
-            <Thing key={1}
+            <Thing
                 x={0} y={0} z={7}
                 width={WIDTH*5} height={HEIGHT}
                 image={"./assets/images/backbackground.jpg"}/>
-            <Thing key={2}
+            <Thing
                 x={0} y={HEIGHT*(1/3)} z={7/2}
                 width={WIDTH*5} height={HEIGHT}
                 image={"./assets/images/background.png"}/>
-                
-            <Thing key={6}
+
+            <Thing
                 x={0} y={0} z={1}
                 width={WIDTH*2} height={HEIGHT}
                 image={"./assets/images/ground1.png"}/>
-            <Thing key={7}
+            <Thing
                 x={WIDTH*2} y={0} z={1}
                 width={WIDTH*2} height={HEIGHT}
                 image={"./assets/images/ground2.png"}/>
-            <Thing key={8}
+            <Thing
                 x={WIDTH*4} y={0} z={1}
                 width={WIDTH*2} height={HEIGHT}
                 image={"./assets/images/ground3.png"}/>
-            <Thing key={3} color="red"
+            <Thing color="red"
                 x={WIDTH*1.5-0.5} y={HEIGHT-6.5} z={1}
                 width={2} height={3}>
                 Hello!
             </Thing>
-            <Thing key={4} color="green"
+            <Thing color="green"
                 x={WIDTH*2.5-0.5} y={HEIGHT-6.5} z={1}
                 width={2} height={3}>
                 Hiya!
             </Thing>
-            <Thing key={5} color="blue"
+            <Thing color="blue"
                 x={WIDTH*3.5-0.5} y={HEIGHT-6.5} z={1}
                 width={2} height={3}>
                 Whatsup?
             </Thing>
+            <Thing color="black"
+                x={WIDTH*4.5-1.5} y={HEIGHT-8.5} z={1}
+                width={4} height={5}/>
         </div>
     ),
     getData: function() {
