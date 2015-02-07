a3384c5facacefed851970605675b7d5ff9a2468
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-07 10:49:45 -0500

    Porting some code to work on mobile devices.

--- source/index.js
+++ source/index.js
@@ -2,11 +2,13 @@ window.React = require("react/addons")
 window.Reflux = require("reflux")
 window.Hammer = new (require("hammerjs"))(document.body)
 
+window.React.initializeTouchEvents(true)
 window.Reflux.StoreMethods.getInitialState = function() {if(this.getData) {return this.getData()}}
 window.Reflux.StoreMethods.retrigger = function() {if(this.getData) {this.trigger(this.getData())}}
 
 window.WIDTH = 15
 window.HEIGHT = 20
+
 
 var Game = require("<scripts>/components/Game")
 React.render(<Game/>, document.body)
